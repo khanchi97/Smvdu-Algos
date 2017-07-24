9d39fc6ae6be6787f2a743fbc59d1dcced2df16d
Nilesh <nileshgulia@gmail.com>
2017-07-24 14:41:45 +0530

    Update binary search.js
    now user can enter array values and search for a given element

--- search-algos/binary_search/binary search.js
+++ search-algos/binary_search/binary search.js
@@ -1,16 +1,10 @@
-inputList = new Array();
-inputList[0] = 'E';
-inputList[1] = 'I';
-inputList[2] = 'O';
-inputList[3] = 'P';
-inputList[4] = 'Q';
-inputList[5] = 'R';
-inputList[6] = 'T';
-inputList[7] = 'U';
-inputList[8] = 'W';
-inputList[9] = 'Y';
-
-function binarySearch(inputList, key) {
+var inputList=[];
+		for(var i=0;i<5;i++){
+		var a=prompt("enter the values");
+		inputList[i]=parseInt(a);}
+		var b=prompt("enter the element to be searched");
+		var c=parseInt(b);
+		function binarySearch(inputList, key) {
     var left = 0;
     var right = inputList.length - 1;
 
@@ -18,7 +12,7 @@ function binarySearch(inputList, key) {
         var mid = parseInt((left + right) / 2);//parseInt converts into integer
 
         if (inputList[mid] == key)
-            return mid;
+            return ("element found at position:"+mid);
         else if (inputList[mid] < key)
             left = mid + 1;
         else
@@ -28,5 +22,5 @@ function binarySearch(inputList, key) {
     return 'Not found';
 }
 
-alert(binarySearch(inputList, 'T'));
-alert(binarySearch(inputList, 'Z')); // Not found
+console.log(binarySearch(inputList, c));//you will get the output in console
+
