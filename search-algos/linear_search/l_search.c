c039fe0aab9eb40bdb2fe85a6a0ce8ea7487cefa
khanchi97 <sushilkhanchi97@gmail.com>
2017-07-26 12:57:12 +0530

    Update linear search implementation

--- search-algos/linear_search/l_search.c
+++ search-algos/linear_search/l_search.c
@@ -2,5 +2,10 @@
 
 int main ()
 {
-            //complete this
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    //now complete this
 }  
