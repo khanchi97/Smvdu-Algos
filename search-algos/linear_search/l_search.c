57c822aaf58a156abab40fccd0c0f59256744bf8
khanchi97 <sushilkhanchi97@gmail.com>
2017-07-28 11:49:57 +0530

    Update linear search

--- search-algos/linear_search/l_search.c
+++ search-algos/linear_search/l_search.c
@@ -2,10 +2,21 @@
 
 int main ()
 {
-    int n;
+    int n,ch;
+    int key;
+    scanf("%d",&key);
+
     scanf("%d",&n);
     int arr[n];
     for (int i=0;i<n;i++)
+    {
         scanf("%d",&arr[i]);
-    //now complete this
+        if(key==arr[i])
+        {
+           ch=i;
+          break;
+        }
+    }
+    //complete this
+
 }  
