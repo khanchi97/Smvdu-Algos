2854c4e1600d581b7254d5ac278c80615e90e7a2
mohit singh <mritunjaygoutam2204@gmail.com>
2017-09-09 15:30:09 +0530

    Update binary search.js

--- search-algos/binary_search/binary search.js
+++ search-algos/binary_search/binary search.js
@@ -2,6 +2,20 @@ var inputList=[];
 		for(var i=0;i<5;i++){
 		var a=prompt("enter the values");
 		inputList[i]=parseInt(a);}
+    	function sort_a(a){
+  	var l,k;
+  	for(l=0;l<(a.length-1);l++){
+   	 for(k=l+1;k<a.length;k++){
+   	   var t;
+	      if(a[l]>a[k]){
+   	     t=a[l];
+  	      a[l]=a[k];
+  	      a[k]=t;
+   	   }
+   	 }
+ 	 }
+	}
+sort_a(inputList);
 		var b=prompt("enter the element to be searched");
 		var c=parseInt(b);
 		function binarySearch(inputList, key) {
