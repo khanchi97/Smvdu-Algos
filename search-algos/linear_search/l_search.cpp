dbc52dee993c98c3cc4806c0c68b01ccc33c1e52
khanchi97 <sushilkhanchi97@gmail.com>
2017-10-25 12:12:46 +0530

    Add linear search in cpp
    issue #51

--- search-algos/linear_search/l_search.cpp
+++ search-algos/linear_search/l_search.cpp
@@ -4,14 +4,24 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n,x;
     cout <<"enter size of array" <<endl;
     cin >>n;
     int arr[n];
-    for(int i=0;i<n;i++)
-        cin >>arr[i];
-
-    //linear search code
-
+    cout << "enter the values" <<endl;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout <<"enter the value you want to find" << endl;
+    cin >> x;
+    for(int i = 0; i < n; i++)
+    {
+      if(arr[i] == x)
+      {
+        cout << "Yes the element is at index\n" << i << endl;
+        break;
+      }
+      if(i == n-1)
+        cout << "No the value you want to find is not present in array" << endl;
+    }
     return 0;
 }
