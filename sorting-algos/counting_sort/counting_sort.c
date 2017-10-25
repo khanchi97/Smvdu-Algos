a7af1e9538cbc48bbac788080579508cf2712da5
Sandeep <sandeepsajan0@gmail.com>
2017-10-25 05:52:20 +0000

    update countingsort.c

--- sorting-algos/counting_sort/counting_sort.c
+++ sorting-algos/counting_sort/counting_sort.c
@@ -1,7 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main ()
-{
-    //complete this
+int main() {
 
+   int n,i,j,k;
+ scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<100;i++){
+        for(j=0;j<n;j++){
+            if(i==a[j]){
+                printf("%d ",i);
+            }
+        }
+    }
+    
+    return 0;
 }
