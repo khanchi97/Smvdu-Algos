5878140fd1bc5a0934bef0df924b8a3a79ef18dd
Awani-Shukla <31660543+Awani-Shukla@users.noreply.github.com>
2017-10-29 19:45:59 +0530

    Update counting sort.java

--- sorting-algos/counting_sort/counting sort.java
+++ sorting-algos/counting_sort/counting sort.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
 
- 
-
-
 
 public class CountingSort 
 
@@ -22,7 +19,6 @@ public class CountingSort
 
             return;
 
-        /** find max and min values **/
 
         int max = arr[0], min = arr[0];
 
@@ -44,15 +40,13 @@ public class CountingSort
 
  
 
-        /** check if range is small enough for count array **/
-
-        /** else it might give out of memory exception while allocating memory for array **/
+       
 
         if (range > MAX_RANGE)
 
         {
 
-            System.out.println("\nError : Range too large for sort");
+            System.out.println("\nError : Range too large");
 
             return;
 
@@ -62,21 +56,16 @@ public class CountingSort
 
         int[] count = new int[range];
 
-        /** make count/frequency array for each element **/
-
+       
         for (int i = 0; i < N; i++)
 
             count[arr[i] - min]++;
 
-        /** modify count so that positions in final array is obtained **/
-
-        for (int i = 1; i < range; i++)
+              for (int i = 1; i < range; i++)
 
             count[i] += count[i - 1];
 
-        /** modify original array **/
-
-        int j = 0;
+              int j = 0;
 
         for (int i = 0; i < range; i++)
 
@@ -86,7 +75,7 @@ public class CountingSort
 
     }    
 
-    /** Main method **/
+   
 
     public static void main(String[] args) 
 
@@ -98,30 +87,23 @@ public class CountingSort
 
         int n, i;
 
-        /** Accept number of elements **/
-
+       
         System.out.println("Enter number of integer elements");
 
         n = scan.nextInt();
 
-        /** Create integer array on n elements **/
-
+        
         int arr[] = new int[ n ];
 
-        /** Accept elements **/
-
-        System.out.println("\nEnter "+ n +" integer elements");
+              System.out.println("\nEnter "+ n +" integer elements");
 
         for (i = 0; i < n; i++)
 
             arr[i] = scan.nextInt();
 
-        /** Call method sort **/
+               sort(arr);
 
-        sort(arr);
-
-        /** Print sorted Array **/
-
+      
         System.out.println("\nElements after sorting ");        
 
         for (i = 0; i < n; i++)
