7cf69b7a8ec6b7873ce35787a7014cf1f83a9196
Muskan Sapolia <muskansapolia@gmail.com>
2017-10-29 16:08:22 +0000

    Update dfs.cpp

--- graph-algos/DFS/dfs.cpp
+++ graph-algos/DFS/dfs.cpp
@@ -1,9 +1,32 @@
-#include<iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
+const int N = 1e5+1;
+bool visited[N];
+vector<int>adj[N];
+
+int dfs(int s)
+{
+  visited[s]=true;
+  for(int i=0;i<adj[s].size();i++)
+    if(visited[adj[s][i]]==false)
+      dfs(adj[s][i]);
+  return 0;
+}
 
 int main()
 {
-    //complete this
-}
+  int nodes,edges,a,b;
+  cin>>nodes>>edges;
+  while(edges--)
+  {
+    cin>>a>>b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+  for(int i=1;i<=nodes;i++)
+    visited[i]=false;
+  for(int i=1;i<=nodes;i++)
+    if(visited[i]==false)
+    dfs(i);
 
+}
