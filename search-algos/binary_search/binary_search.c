b369631045b3fb6e6f898a68cdd3b7b7bf7fd154
Gurjot Singh Suri <anshusuri123@gmail.com>
2017-12-04 20:09:05 +0530

    Modified binary_search.c to return original index in unsorted array

--- search-algos/binary_search/binary_search.c
+++ search-algos/binary_search/binary_search.c
@@ -1,22 +1,36 @@
-//gurjot273
+
 
 #include <stdio.h>
 
-//using insertion sort for producing sorted array for binary search
+//using selection sort for producing sorted array for binary search
 
-void insertionsort(int A[],int n)
+void selsort(int A[],int oindexA[],int size)
 {
-	int i,j,key;
+	int flag=0,temp,min,i,j;
 
-	for(i=1;i<n;i++)
-		{
-			key=A[i];
-			for(j=i-1;j>=0 && A[j]>key;j--)
-				{A[j+1]=A[j];}
+			for(i=0;i<size;i++)
+				{	min=A[i];
+					flag=i;
+					for(j=i+1;j<size;j++)
+				{
+					if(A[j]<min) {
+                                    min=A[j];
+                                    flag=j;
+                                }
+				}
+			temp=A[i];
 
-				A[j+1]=key;
+            A[i]=min;
+			A[flag]=temp;
+
+			temp=oindexA[i];
+			oindexA[i]=oindexA[flag]; //original index of A[i] after swapping in unsorted array
+			oindexA[flag]=temp; //original index of A[flag] after swapping in unsorted array
+
+
 		}
 }
+
 
 
 int binarysearch(int A[],int low,int high,int key)
@@ -44,7 +58,11 @@ void main()
 
     int i;
 
-    int A[size],key;
+    int A[size],oindexA[size],key;//array oindex stores the index in unsorted array corresponding to index in sorted array
+
+    for(i=0;i<size;i++)
+        oindexA[i]=i;    //initialising the original index array
+
 
     printf("Enter the elements of array\n");
 
@@ -55,14 +73,18 @@ void main()
 
     scanf("%d",&key);
 
-    insertionsort(A,size);  //sortiing by insertion sort for binary search
+    selsort(A,oindexA,size);  //sortiing by selection sort for binary search and storing original indices in array oindexA
 
     int index=binarysearch(A,0,size-1,key);//calling binarysearch function
+
+    int originalindex=oindexA[index]; //converts index from sorted array to index in unsorted array
 
     if(index==-1)
         printf("Element %d not found in the array\n",key);
     else
-        printf("Element %d found in the sorted array at index %d",key,index);
+        printf("Element %d found in the sorted array at index %d",key,originalindex); //displays index with respect to unsorted array
+
+    //gurjot273
 }
 
 
