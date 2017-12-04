56c8a88d70f5bc9580fd8e0a94728fad64f56873
RajaVamsi11 <30550059+RajaVamsi11@users.noreply.github.com>
2017-12-04 18:47:31 +0530

    Update l_search.c

--- search-algos/linear_search/l_search.c
+++ search-algos/linear_search/l_search.c
@@ -1,43 +1,34 @@
 
 #include<stdio.h>
-void main()
-{
-   int flag,n,x;
+void main(){
+   int flag,n,x,pos;
    flag=0;
    printf("Enter the length of an Array\n");  
    scanf("%d",&n);                                   //Taking length of an array from user
    int a[n];                                          //Intializing an array of length n
    printf("Enter %d value \n",n);
 
-   
    for(int i=0;i<n;i++){
-scanf("%d",&a[i]);                                     //Taking n array elements from the user
+    scanf("%d",&a[i]);                                     //Taking n array elements from the user
    }                                                    
    
    printf("Enter the value to be find :");  
    scanf("%d",&x);                                      //Element to be find
    
                                                          
-   for(i=0;i<n;i++){                                      //Linear search logic
+      for(int i=0;i<n;i++){                                 //Linear search logic                                                      
 
-   for(i=0;i<n;i++)
-scanf("%d",&a[i]);                                        //Taking input  array elements from user
-   scanf("%d",&n);
-   printf("Which value to be search :");
-   scanf("%d",&x);                                             //Element to be find
-   scanf("%d",&n);
-                                                            //Linear search logic
-
-      if(x==a[i])
-      {
- flag=1;
- break;
+      if(x==a[i]){
+         flag=1;
+         pos=i;
+          break;
       }
+         
    }
    if(flag==0)
       printf("%d value not found\n",x);                     //Not found ..Print Element not exist in given array
    else
-      printf("%d value found at location %d\n",x,i);          //found..Print Found and the position af the value in array
-   getch();
+      printf("%d value found at location %d\n",x,pos);          //found..Print Found and the position af the value in array
+ 
 }
 
