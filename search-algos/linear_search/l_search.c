1be14703844b3edb6825920162992e891f8154d6
RajaVamsi11 <30550059+RajaVamsi11@users.noreply.github.com>
2017-12-04 13:50:29 +0530

    Update l_search.c
    solved my mistakes

--- search-algos/linear_search/l_search.c
+++ search-algos/linear_search/l_search.c
@@ -3,20 +3,21 @@
 void main()
 {
    int flag,n,x;
+   flag=0;
    printf("Enter the length of an Array\n");  
-   scanf("%d",&n);                                   \\Taking length of an array from user
-   int a[n];                                          \\Intializing an array of length n
+   scanf("%d",&n);                                   //Taking length of an array from user
+   int a[n];                                          //Intializing an array of length n
    printf("Enter %d value \n",n);
    
    for(int i=0;i<n;i++){
-scanf("%d",&a[i]);                                     \\Taking n array elements from the user
+scanf("%d",&a[i]);                                     //Taking n array elements from the user
    }                                                    
    
    printf("Enter the value to be find :");  
-   scanf("%d",&x);                                      \\Element to be find
+   scanf("%d",&x);                                      //Element to be find
    
                                                          
-   for(i=0;i<n;i++){                                      \\Linear search logic
+   for(i=0;i<n;i++){                                      //Linear search logic
       if(x==a[i])
       {
  flag=1;
@@ -24,9 +25,9 @@ scanf("%d",&a[i]);                                     \\Taking n array elements
       }
    }
    if(flag==0)
-      printf("%d value not found\n",x);                     \\Not found ..Print Element not exist in given array
+      printf("%d value not found\n",x);                     //Not found ..Print Element not exist in given array
    else
-      printf("%d value found at location %d\n",x,i);          \\ found..Print Found and the position af the value in array
+      printf("%d value found at location %d\n",x,i);          //found..Print Found and the position af the value in array
    getch();
 }
 
