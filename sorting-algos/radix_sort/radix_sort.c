d7eda1f7e3e9a8ca00c3a9fd3744ac59efce2752
Gurjot Singh Suri <anshusuri123@gmail.com>
2017-12-10 13:06:40 +0530

    Replaced empty radix_sort.c with implementation

--- sorting-algos/radix_sort/radix_sort.c
+++ sorting-algos/radix_sort/radix_sort.c
@@ -1,7 +1,68 @@
-#include<stdio.h>
+#include <stdio.h>
 
+void counting_sort(int arr[],int size,int exp)
+{
+    int aux[size];
+    int freq[10];
+    int i;
+    for(i=0;i<10;i++)
+        freq[i]=0;
+    int temp;
+    for(i=0;i<size;i++)
+    {
+        temp=(arr[i]/exp)%10;
+        freq[temp]++;
+    }
+
+    for(i=1;i<10;i++)
+        freq[i]+=freq[i-1];
+
+    for(i=size-1;i>=0;i--)
+    {
+        temp=(arr[i]/exp)%10;
+        aux[freq[temp]-1]=arr[i];
+        freq[temp]--;
+    }
+
+    for(i=0;i<size;i++)
+        arr[i]=aux[i];
+}
+
+void radix_sort(int arr[],int size)
+{
+    int i;
+    int exp=1;
+    int max=arr[0];
+    for(i=1;i<size;i++)
+    {
+        if(max<arr[i])
+            max=arr[i];
+    }
+    int digit =0;
+    int temp=max;
+    while(temp!=0)
+    {
+        digit++;
+        temp/=10;
+    }
+    for(i=0;i<digit;i++)
+    {
+        counting_sort(arr,size,exp);
+        exp*=10;
+    }
+}
 int main ()
 {
-    //complete this
-
+    int size,i;
+    printf("Enter the number of elements in array\n");
+    scanf("%d",&size);
+    int arr[size];
+    printf("Enter the elements of array\n");
+    for(i=0;i<size;i++)
+        scanf("%d",&arr[i]);
+    radix_sort(arr,size);
+    printf("The sorted array is \n");
+    for(i=0;i<size;i++)
+        printf("%d ",arr[i]);
+    printf("\n");
 }
