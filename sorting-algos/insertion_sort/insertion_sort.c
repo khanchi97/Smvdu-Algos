e19bd8f090b3fac083ee152e09957b486bea33e9
Utkarsh Gupta <32920299+utkarshg6@users.noreply.github.com>
2018-03-28 15:36:21 +0530

    Added Insertion Sort

--- sorting-algos/insertion_sort/insertion_sort.c
+++ sorting-algos/insertion_sort/insertion_sort.c
@@ -1,7 +1,28 @@
+// Insertion Sort
 #include<stdio.h>
-
-int main ()
+void main()
 {
-    //complete this
-
+    int n,i, j, key;
+    printf("Enter Size of an Array : ");
+    scanf("%d",&n);
+    int a[n];
+    for ( i=0; i< n; i++)                                 // Input in Array
+    {
+        printf("Enter Element Number %d :",(i+1));
+        scanf("%d",&a[i]);
+    }
+    for ( i=1; i< n; i++ )
+    {
+        key = a[i];                        //Starts from 2nd element of array a.
+        j = i-1;                           // j is the previous element.
+        while ( a[j]>=key && j>=0 )          // If previous elements are greater and pointer is not at the end.
+        {
+            a[j+1] = a[j];                 // Shift all the elements to right side.
+            j -= 1;                        // Decrement j
+        }
+        a[j+1] = key;                        // return key to its new position.
+    }
+    printf("Sorted Array --> \n");
+    for ( i=0; i<n; i++ )
+        printf("%d ",a[i]);
 }
